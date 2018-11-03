0a112a85b1cd73f9e302e25a67189bd01958f778
Ubuntu <ubuntu@ip-192-168-1-93.ec2.internal>
2018-11-03 07:08:03 +0000

    added counter

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const express = require('express');
 // Constants
 const PORT = 8081;
 const HOST = '0.0.0.0';
+var counter = 0
 
 // App
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello world\n');
+  counter = counter + 1; 	 
+  console.log(`counter is ${counter}`);	 
 });
 
 app.listen(PORT, HOST);
